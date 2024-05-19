c003ce32a7be1f6b59db8e8714dcd18d19d61e2f
kawanars <153693747+kawanars@users.noreply.github.com>
2024-05-19 19:59:02 -0400

    Add files via upload

---
+++ resources/project_schema.sql
@@ -0,0 +1,47 @@
+DROP TABLE IF EXISTS project_category;
+DROP TABLE IF EXISTS step;
+DROP TABLE IF EXISTS category;
+DROP TABLE IF EXISTS step;
+DROP TABLE IF EXISTS material;
+DROP TABLE IF EXISTS project;
+
+CREATE TABLE project(
+project_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+project_name VARCHAR(128) NOT NULL,
+estimated_hours decimal (7,2),
+actual_hours decimal(7,2),
+difficulty INT,
+notes text
+);
+
+
+CREATE TABLE material(
+material_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+project_id INT NOT NULL,
+material_name VARCHAR(128) NOT NULL,
+num_required INT,
+cost decimal(7,2),
+FOREIGN KEY (project_id) REFERENCES project (project_id) ON DELETE CASCADE
+);
+
+
+CREATE TABLE step (
+step_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+project_id INT NOT NULL,
+step_text text NOT NULL,
+step_order INT NOT NULL,
+FOREIGN KEY (project_id) REFERENCES project (project_id) ON DELETE CASCADE
+);
+
+CREATE TABLE category(
+category_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+category_name VARCHAR(128) NOT NULL
+);
+
+
+CREATE TABLE project_category(
+project_id INT NOT NULL UNIQUE,
+category_id INt NOT NULL UNIQUE,
+FOREIGN KEY (project_id) REFERENCES project (project_id) ON DELETE CASCADE,
+FOREIGN KEY (category_id) REFERENCES category (category_id) ON DELETE CASCADE
+);
